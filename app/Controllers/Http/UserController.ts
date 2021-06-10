9114190e06826e9095f4d15f501b72c8789c48ab
ViniciusTei <viniciustprates@gmail.com>
2021-06-10 11:33:02 -0300

    ✨add user controller

--- app/Controllers/Http/UserController.ts
+++ app/Controllers/Http/UserController.ts
@@ -1,24 +1,27 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class UserController {
   public async index() {
-    
-    return [
-      {
-        id: 0,
-        name: "Vinicius Teixeira",
-        email: "viniteirapa@gmail.com",
-        image: ""
-      },
-    ]
+    const user = await User.all()
+    return user
   }
 
   public async create(ctx: HttpContextContract) {
-      const body = ctx.request.body
-
-      console.log(body)
-      return {
-          create: true
+      const body = ctx.request.body()   
+      
+      try {
+        const user = await User.create({
+          id: body.userId,
+          email: body.email,
+          name: body.name,
+          image: body.image
+        })
+        return user.$isPersisted
+      } catch (error) {
+        console.log(error)
+        return false
       }
+    
   }
 }
