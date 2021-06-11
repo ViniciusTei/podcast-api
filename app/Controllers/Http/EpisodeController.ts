05df8e97870180536c2373deb68b3d1db7cec697
ViniciusTei <viniciustprates@gmail.com>
2021-06-11 12:20:17 -0300

    🐛 fix episodes pagination

--- app/Controllers/Http/EpisodeController.ts
+++ app/Controllers/Http/EpisodeController.ts
@@ -8,7 +8,7 @@ export default class EpisodeContoller {
         const { page } = request.params()
         const limit = 15
 
-        const episodes = await Episode.all()
+        const episodes = await Database.from('episodes').paginate(page, limit)
 
         return response.status(200).send({
             message: "Sucessa",
