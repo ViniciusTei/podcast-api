e555a2f0b201604c3aa7e130b375b413d7e28ad0
ViniciusTei <viniciustprates@gmail.com>
2021-06-11 09:01:02 -0300

    ✨ delete episode and pagination

--- app/Controllers/Http/EpisodeController.ts
+++ app/Controllers/Http/EpisodeController.ts
@@ -1,11 +1,31 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 import Episode from "App/Models/Episode"
 
 export default class EpisodeContoller {
-    public async index() {
+    public async index({request, response}: HttpContextContract) {
+        const { page } = request.params()
+        const limit = 15
+
+        const episodes = Database.from('episodes').paginate(page, limit)
+
+        return response.status(200).send({
+            message: "Sucessa",
+            data: episodes
+        })
+    }
+
+    public async deleteAll({response}: HttpContextContract) {
         const episodes = Episode.all()
 
-        return episodes
+        ;(await episodes).forEach(async episode => {
+            await episode.delete()
+        })
+
+        return response.status(200).send({
+            message: "Success",
+            data: "Deleted!"
+        })
     }
 }
