19c21d065695eb5f30f9d250562f39fb22b55093
ViniciusTei <viniciustprates@gmail.com>
2025-05-07 10:38:26 -0300

    fix: deploys script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "Starting Deploy.."
-sudo pm2 stop podcastr-api
+pm2 stop podcastr-api
 echo "Installing dependencies.."
 npm install
 echo "Running build.."
 npm run build
 echo "Restarting server.."
-sudo pm2 restart podcastr-api
-sudo service nginx restart
+pm2 restart podcastr-api
+service nginx restart
 echo "Success!"
