f790b945d4bf34d71150c90ea4cd9b34d45c06a6
ViniciusTei <viniciustprates@gmail.com>
2025-05-07 09:07:48 -0300

    build: deploy files and execute deploy comand

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-echo "Deploying.."
-sudo pm2 stop api
-echo "Fetching Git.."
-sudo git fetch
-sudo git reset --hard origin/master
+echo "Starting Deploy.."
+sudo pm2 stop podcastr-api
 echo "Installing dependencies.."
 npm install
 echo "Running build.."
 npm run build
 echo "Restarting server.."
-sudo pm2 restart api
+sudo pm2 restart podcastr-api
 sudo service nginx restart
 echo "Success!"
